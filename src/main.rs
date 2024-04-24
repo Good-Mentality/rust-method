4223f75a618c00ed2baf1998bbcbbf859977fc88
Good-Mentality <hasgol92@gmail.com>
2024-04-24 04:10:36 +0300

    Associated function implementation template on a type created

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn create_square(side: u32) -> Self {
+        Self {
+            width: side,
+            height: side,
+        }
+    }
 }
 
 fn main() {
@@ -17,6 +24,8 @@ fn main() {
     };
 
     let area = rec.area();
+    let square = Rectangle::create_square(5);
 
     println!("The area of {:#?} rectangle is equal to {area}", rec);
+    println!("Our craeted square has these specifications: {:#?}", square);
 }
